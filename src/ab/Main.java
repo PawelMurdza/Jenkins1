899b0a187d2910e3d35110699a5215472a3017f3
PawelMurdza <pawel.murdza@gmail.com>
2022-06-21 21:09:24 -0400

    Jenkins

---
+++ src/ab/Main.java
@@ -0,0 +1,9 @@
+package ab;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=0 ;i<5 ;i++){
+            System.out.println("hello! am i a cat?");
+        }
+    }
+}
